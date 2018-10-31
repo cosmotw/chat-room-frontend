f662050642bd526c84e492040cca36d39aa509ab
Cosmo <shooliss@gmail.com>
2018-10-31 17:44:11 +0800

    Fixed eslint rule issue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'vuetify/dist/vuetify.min.css';
 import Vue from 'vue';
+import Vuetify from 'vuetify';
 import App from './App';
 import router from './router';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify);
 
@@ -14,6 +14,5 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>',
+  render: h => h(App),
 });
